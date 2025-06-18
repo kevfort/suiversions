28e90a04529761be0c4aa292a55682d709ef3e70
kevfort <193007420+kevfort@users.noreply.github.com>
2025-06-18 11:39:11 -0400

    initial commit

---
+++ test.py
@@ -0,0 +1,51 @@
+import requests
+import re
+"""
+endpoint = "https://api.github.com/repos/{owner}/{repo_name}/releases/latest"
+
+response = requests.get(endpoint.format(owner="MystenLabs",repo_name="sui"))
+
+if response.status_code == 200:
+    data = response.json()
+    print(data['name'])"""
+    # TODO iterate releases
+    # TODO Robust if mainnet not found... fatal error.
+def get_version_number(owner, repo_name):
+    endpoint = "https://api.github.com/repos/{owner}/{repo_name}/releases"
+    response = requests.get(endpoint.format(owner=owner,repo_name=repo_name))
+    version_types = {
+        "testnet": None,
+        "devnet": None,
+        "mainnet": None
+    }
+    if response.status_code == 200:
+        data = response.json()
+        for release in data:
+            
+            if version_types["testnet"] is None:
+                if "testnet" in release["tag_name"]:
+                    version_types["testnet"] = release["tag_name"]
+
+            elif version_types["devnet"] is None:
+                if "devnet" in release["tag_name"]:
+                    version_types["devnet"] = release["tag_name"]
+
+            elif version_types["mainnet"] is None:
+                if "mainnet" in release["tag_name"]:
+                    version_types["mainnet"] = release["tag_name"]
+                    
+
+            else:
+                print("All versions found")
+                break
+
+    elif response.status_code == 404:
+        return("Not Found")
+
+    if list(version_types.values()).count(None) == 0:
+        return(version_types) 
+    
+    else:
+        return("1") #One or more of the versions were not found
+
+print(get_version_number("MystenLabs", "sui"))
